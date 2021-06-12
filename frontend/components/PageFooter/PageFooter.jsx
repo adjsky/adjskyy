0a432203acd1cf881f4164157e8cfb09aaa08e8e
adjsky <lopr1965@gmail.com>
2021-06-12 23:19:43 +1000

    fix styles

--- frontend/components/PageFooter/PageFooter.jsx
+++ frontend/components/PageFooter/PageFooter.jsx
@@ -5,12 +5,14 @@ const ContactsSepImg = require("../../assets/separator.png")
 function PageFooter() {
   return (
     <footer className="page-footer">
-      <small className="page-footer_copyright">COPYRIGHT 2021 adjsky</small>
-      <ul className="page-footer_contacts">
-        <li><a href="https://vk.com/wannabe2dxd" target="_blank" rel="noreferrer">vk</a></li>
-        <li><img src={ContactsSepImg} alt="" /></li>
-        <li><a href="https://github.com/adjsky/" target="_blank" rel="noreferrer">github</a></li>
-      </ul>
+      <div className="page-footer_row">
+        <small className="page-footer_copyright">COPYRIGHT 2021 adjsky</small>
+        <ul className="page-footer_contacts">
+          <li><a href="https://vk.com/wannabe2dxd" target="_blank" rel="noreferrer">vk</a></li>
+          <li><img src={ContactsSepImg} alt="" /></li>
+          <li><a href="https://github.com/adjsky/" target="_blank" rel="noreferrer">github</a></li>
+        </ul>
+      </div>
     </footer>
   )
 }
